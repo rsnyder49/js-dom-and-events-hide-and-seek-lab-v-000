a59a1a18f1ede55984bbcf202cce0f50ae1e8da1
Robert Snyder <r.snyder49@gmail.com>
2018-12-27 01:50:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll('.ranked-list');
+  
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i]) + n;
+  }
+}
+
+function deepestChild() {
+  let child = document.querySelectorAll('#grand-node div');
+  return child[child.length - 1];
+}
